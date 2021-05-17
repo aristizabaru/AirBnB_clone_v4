cf54c74e707cb0506ed281d034675b61cfafdd56
tomasgvgt <tomasgvg.t@gmail.com>
2021-05-16 19:57:55 -0500

    documentation

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,3 +1,12 @@
+/*
+JavaScript script (static/scripts/2-hbnb.js):
+
+    Based on 1-hbnb.js
+    Request http://0.0.0.0:5001/api/v1/status/:
+        If in the status is “OK”, add the class available to the DIV#api_status
+        Otherwise, remove the class available to the DIV#api_status
+*/
+
 const HOST = '0.0.0.0';
 const amenityData = {};
 
